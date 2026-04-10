4921bfea86266cb026d3902a8c2bcf9d9cf85620
maaliksaad <saadaxis51@gmail.com>
2026-04-10 22:35:38 +0500

    portfolio update

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -2,15 +2,26 @@ import { type ExperienceShowcaseListItemProps } from "@/components/experience/ex
 
 export const EXPERIENCE: ExperienceShowcaseListItemProps[] = [
   {
-    title: "Frontend Developer",
+    title: "Full Stack Developer",
     organisation: {
-      name: "Voxtesy",
-      href: "https://www.voxtesy.com/login",
+      name: "RemoteFlow",
+      href: "https://www.remoteflow.io/",
     },
-    date: "Jan 2024 - Mar 2025",
+    date: "Sep 2025 - Present",
     location: "Remote",
     description:
-      "As a Frontend Developer at Voxtesy, I utilize technologies such as ReactJS, NextJS to create intuitive and dynamic user interfaces. I leverage NodeJS for efficient server-side operations and TailwindCSS to design responsive and visually appealing web applications. My role involves collaborating with cross-functional teams to deliver high-quality, scalable solutions that enhance user experience and drive business growth.",
+      "As a Full Stack Developer at RemoteFlow, I utilize technologies such as ReactJS, NextJS,NestJS, PostgreSQL, TypeScript and more to Built responsive, interactive UIs using modern frontend technologies. Developed robust backend services with NestJS, implementing RESTful APIs, authentication, and authorization workflows.Integrated third-party APIs and services, implementing error handling, retry logic, and rate limiting strategies. Managed application state across the stack, from frontend state management to backend session handling.",
+  },
+  {
+    title: "Frontend Developer",
+    organisation: {
+      name: "Smartan Solver",
+      href: "https://www.smartansolver.site/",
+    },
+    date: "Oct 2024 - Sep 2025",
+    location: "On-Site",
+    description:
+      "As a Frontend Developer at Smartan Solver, I utilize technologies such as ReactJS, NextJS to create intuitive and dynamic user interfaces. I leverage NodeJS for efficient server-side operations and TailwindCSS to design responsive and visually appealing web applications. My role involves collaborating with cross-functional teams to deliver high-quality, scalable solutions that enhance user experience and drive business growth.",
   },
   {
     title: "Intern",
